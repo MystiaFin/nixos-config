c76cd6eda1765f63d897ad730a694ddfb03a3e06
MystiaFin <cytrsx01@gmail.com>
2026-01-03 19:51:27 +0700

    commit

--- home-manager/niri.nix
+++ home-manager/niri.nix
@@ -1,14 +1,14 @@
 { config, pkgs, device, ... }:
 
 let
-  terminalCmd = if device == "low-end" then "foot" else "kitty";
+  terminalCmd = if device == "thinkpad" then "foot" else "kitty";
   rawConfig = builtins.readFile ./config/niri.kdl;
   finalConfig = builtins.replaceStrings [ "@TERMINAL@" ] [ terminalCmd ] rawConfig;
 in
 
 {
   xdg.configFile."niri/config.kdl".text = ''
-    ${if device == "low-end" then ''
+    ${if device == "thinkpad" then ''
       output "eDP-1" {
           mode "1337x768@60"
           scale 1.0
