2b0b7410756ec063b73d9aeffbca5579cf7e7e5c
MystiaFin <cytrsx01@gmail.com>
2026-01-25 16:34:09 +0700

    fix cava

--- home-manager/cava.nix
+++ home-manager/cava.nix
@@ -13,17 +13,6 @@
 
     [output]
     method = noncurses
-
-    [color]
-    gradient = 1
-    gradient_color_1 = '#59cc33'
-    gradient_color_2 = '#80cc33'
-    gradient_color_3 = '#a6cc33'
-    gradient_color_4 = '#cccc33'
-    gradient_color_5 = '#cca633'
-    gradient_color_6 = '#cc8033'
-    gradient_color_7 = '#cc5933'
-    gradient_color_8 = '#cc3333'
   '';
 
   xdg.configFile."cava/config-raw".text = ''
